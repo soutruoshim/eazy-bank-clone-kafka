b0a930a748dab4c7aa414f0530a36dd8bde5750b
SrhDP <>
2024-05-11 09:27:01 +0700

    config yml gateway server

--- gatewayserver/src/main/java/com/eazybytes/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/eazybytes/gatewayserver/GatewayserverApplication.java
@@ -14,5 +14,20 @@ public class GatewayserverApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayserverApplication.class, args);
 	}
-
+	@Bean
+	public RouteLocator eazyBankRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+		return routeLocatorBuilder.routes()
+				.route(p -> p
+						.path("/eazybank/accounts/**")
+						.filters( f -> f.rewritePath("/eazybank/accounts/(?<segment>.*)","/${segment}"))
+						.uri("lb://ACCOUNTS"))
+				.route(p -> p
+						.path("/eazybank/loans/**")
+						.filters( f -> f.rewritePath("/eazybank/loans/(?<segment>.*)","/${segment}"))
+						.uri("lb://LOANS"))
+				.route(p -> p
+						.path("/eazybank/cards/**")
+						.filters( f -> f.rewritePath("/eazybank/cards/(?<segment>.*)","/${segment}"))
+						.uri("lb://CARDS")).build();
+	}
 }
