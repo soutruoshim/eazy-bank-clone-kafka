adb2d4a6ef34a3c512fbe90754ceef2b884060c8
SrhDP <>
2024-05-11 08:58:07 +0700

    init gateway server

---
+++ gatewayserver/src/main/java/com/eazybytes/gatewayserver/GatewayserverApplication.java
@@ -0,0 +1,18 @@
+package com.eazybytes.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+
+@SpringBootApplication
+public class GatewayserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+
+}
